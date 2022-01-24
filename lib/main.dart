d67db874475f17e3822d3658a4b0cead60daa54a
maeva <maeganelys@gmail.com>
2022-01-24 13:41:50 +0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text('Calculateur IMC')),
+          backgroundColor: Colors.lightBlue,
+        ),
+        body: Center(
+
+),
+          ),
+    ),
+  );
+}
