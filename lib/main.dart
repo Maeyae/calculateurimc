0f3d330780707339624db0c6515e278387032be1
maeva <maeganelys@gmail.com>
2022-01-26 11:33:33 +0400

    add button

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
+  runApp(const Myapp());
+}
+
+class Myapp extends StatefulWidget {
+  const Myapp({Key? key}) : super(key: key);
+
+  @override
+  _MyappState createState() => _MyappState();
+}
+class _MyappState extends State<Myapp> {
+
+
+  @override
+      Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
           title: Center(child: Text('Calculateur IMC')),
           backgroundColor: Colors.lightBlue,
         ),
         body: Center(
+          child: Text("calcul en cours..."),
+        ),
+      floatingActionButton: FloatingActionButton (
+        child: Icon(Icons.add),
+     onPressed: (){},
 
-),
-          ),
-    ),
-  );
+      ),
+          );
+
+  }
 }
